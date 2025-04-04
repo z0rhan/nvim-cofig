1735437138ca66fd8f92b6541ebaefb4c1da630b
z0rhan <uknowme1928@gmail.com>
2025-04-04 17:35:52 +0300

    Customize nvim-cmp, Configure cmp-cmdline

--- init.lua
+++ init.lua
@@ -24,11 +24,42 @@ vim.keymap.set('v', '<Leader>y', '"+y', { noremap = true, silent = true })
 
 require("config.lazy")
 
+-- For transparent background
 vim.cmd([[hi! Normal guibg=NONE ctermbg=NONE]])
 vim.cmd([[hi! NormalNC guibg=NONE ctermbg=NONE]])
-  -- Add more highlighting groups as needed
+-- Add more highlighting groups as needed
 
+-- Vertical mark
+vim.opt.colorcolumn = "80,100"
+
+
+-- Highlight for nvim-cmp
 -- Enable persistent undo
 vim.opt.undodir = vim.fn.expand('~/.config/nvim/undo')  -- Set undo directory
 vim.opt.undofile = true  -- Enable undo file support
 
+-- Deprecated items (gray)
+vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#928374' }) -- Gruvbox gray
+
+-- Matching text (yellow/orange)
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#fabd2f' }) -- Gruvbox yellow
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpItemAbbrMatch' }) -- Fixed typo in your original
+
+-- Variables, interfaces, text (blue)
+vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#83a598' }) -- Gruvbox blue
+vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
+vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
+
+-- Functions/methods (purple)
+vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#d3869b' }) -- Gruvbox purple
+vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
+
+-- Keywords, properties, units (foreground)
+vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#ebdbb2' }) -- Gruvbox foreground
+vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
+vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
+
+-- Additional Gruvbox-themed highlights
+vim.api.nvim_set_hl(0, 'CmpItemKindClass', { bg='NONE', fg='#fabd2f' }) -- Yellow
+vim.api.nvim_set_hl(0, 'CmpItemKindModule', { bg='NONE', fg='#8ec07c' }) -- Green
+vim.api.nvim_set_hl(0, 'CmpItemKindSnippet', { bg='NONE', fg='#fe8019' }) -- Orange
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,8 +1,9 @@
 return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 	},
 	{
 		"L3MON4D3/LuaSnip",
@@ -12,9 +13,17 @@ return {
 		},
 	},
 	{
+		"onsails/lspkind.nvim",
+		config = function()
+			require("lspkind").setup()
+		end,
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -23,10 +32,7 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -34,11 +40,59 @@ return {
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
+
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
 					{ name = "luasnip" }, -- For luasnip users.
-				}, {
 					{ name = "buffer" },
+					{ name = "path" },
+				}),
+
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+
+				formatting = {
+					format = function(entry, vim_item)
+						if vim.tbl_contains({ "path" }, entry.source.name) then
+							local icon, hl_group =
+								require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+							if icon then
+								vim_item.kind = icon
+								vim_item.kind_hl_group = hl_group
+								return vim_item
+							end
+						end
+						return require("lspkind").cmp_format({
+                            mode = "symbol_text",
+							menu = {
+								buffer = "[Buffer]",
+								nvim_lsp = "[LSP]",
+								luasnip = "[LuaSnip]",
+								nvim_lua = "[Lua]",
+								latex_symbols = "[Latex]",
+							},
+						})(entry, vim_item)
+					end,
+				},
+
+				cmp.setup.cmdline({ "/", "?" }, {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = {
+						{ name = "buffer" },
+					},
+				}),
+
+				cmp.setup.cmdline(":", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = "path" },
+					}, {
+						{ name = "cmdline" },
+					}),
+					matching = { disallow_symbol_nonprefix_matching = false },
 				}),
 			})
 		end,
